a4ed022e4b63ba7c36a6b3b8fda142bb598c35a6
Elec332 <arnout1998@gmail.com>
2014-12-05 18:18:47 +0100

    Added new way of versioning

--- src/main/java/elec332/core/PSF/modInfo.java
+++ src/main/java/elec332/core/PSF/modInfo.java
@@ -2,7 +2,6 @@ package elec332.core.PSF;
 
 public class modInfo {
 	
-	public static final String VERSION = "0.0.1";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1230,)";
 	public static final String ACCEPTEDMCVERSIONS = "[1.7.10]";
 	public static final String CLIENTPROXY = "elec332.core.proxies.ClientProxy";
--- src/main/java/elec332/core/main/ElecCore.java
+++ src/main/java/elec332/core/main/ElecCore.java
@@ -8,11 +8,11 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import elec332.core.PSF.*;
 import elec332.core.config.ConfigCore;
 import elec332.core.proxies.CommonProxy;
+import elec332.core.util.MCModInfo;
 
 
-
-@Mod(modid = modInfo.MODID_CORE, name = modInfo.MODNAME_CORE, version = modInfo.VERSION, dependencies = modInfo.DEPENDENCIES,
-acceptedMinecraftVersions = modInfo.ACCEPTEDMCVERSIONS)
+@Mod(modid = modInfo.MODID_CORE, name = modInfo.MODNAME_CORE, dependencies = modInfo.DEPENDENCIES,
+acceptedMinecraftVersions = modInfo.ACCEPTEDMCVERSIONS, useMetadata = true, canBeDeactivated = false)
 public class ElecCore{
 	
 	//EXP	
@@ -28,6 +28,10 @@ public class ElecCore{
 	public void preInit(FMLPreInitializationEvent event) {
 		ConfigCore.preInit(event.getModConfigurationDirectory());
 		//config.load();
+		MCModInfo.CreateMCModInfo(event, modInfo.MODID_CORE, modInfo.MODNAME_CORE, "Created by Elec332",
+				"Provides core functionality for Elec's Mods",
+				"-", "assets/elec332/logo.png",
+				new String[]{"Elec332"});
 	}
 	
 	
