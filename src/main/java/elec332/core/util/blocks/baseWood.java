fc9def9c18f194e90522e03a13455589dcdd8b20
Elec332 <arnout1998@gmail.com>
2014-12-20 18:41:13 +0100

    Replace depricated method

--- src/main/java/elec332/core/util/blocks/baseWood.java
+++ src/main/java/elec332/core/util/blocks/baseWood.java
@@ -3,8 +3,8 @@ package elec332.core.util.blocks;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Created by Elec332 on 19-12-2014.
@@ -13,12 +13,35 @@ public class baseWood extends baseblock{
 
    public baseWood(String blockName, CreativeTabs CreativeTab, FMLPreInitializationEvent event) {
         super(Material.wood, blockName, CreativeTab, event, 2);
-        Blocks.fire.func_149842_a(getIdFromBlock(this), 5, 20);
     }
 
     @Override
-    public boolean isWood(IBlockAccess world, int x, int y, int z)
-    {
+    public int getFlammability(IBlockAccess world, int x, int y, int z, ForgeDirection face) {
+        return 20;
+    }
+
+    @Override
+    public boolean isFlammable(IBlockAccess world, int x, int y, int z, ForgeDirection face) {
+        return true;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(IBlockAccess world, int x, int y, int z, ForgeDirection face) {
+        if (face == ForgeDirection.DOWN)
+            return 20;
+        else if (face != ForgeDirection.UP)
+            return 10;
+        else
+            return 5;
+    }
+
+    @Override
+    public boolean canSustainLeaves(IBlockAccess world, int x, int y, int z) {
+        return true;
+    }
+
+    @Override
+    public boolean isWood(IBlockAccess world, int x, int y, int z) {
         return true;
     }
 }
