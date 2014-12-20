8b81b87cbda41308177a16bde69639441b205c2d
Elec332 <arnout1998@gmail.com>
2014-12-20 15:55:25 +0100

    Depricate old createItem method
    Items werent met to be made that way ;)

--- src/main/java/elec332/core/util/old/basicItem.java
+++ src/main/java/elec332/core/util/old/basicItem.java
@@ -1,17 +1,19 @@
-package elec332.core.util.Regist;
+package elec332.core.util.old;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import elec332.core.helper.registerHelper;
 import elec332.core.main.ElecCTab;
 import net.minecraft.item.Item;
 
-public class ItemRegist extends Item{
+public class basicItem extends Item{
 	
 	public static Item Item;
 
 	private static void registerItem(Item mItem, String name) {
 		GameRegistry.registerItem(mItem, name);
 	}
-	
+
+	@Deprecated
 	protected static void CreateItem(Item item, String Itemname, int MaxStackSize) {
 		SetStack(item, MaxStackSize);
 		item.setCreativeTab(ElecCTab.ElecTab);
