5335fdfff0d280a63aabb6488d3c76b12e0e2959
Elec332 <arnout1998@gmail.com>
2014-12-21 17:48:24 +0100

    Added Registry and handler for furnaceMod

---
+++ src/main/java/elec332/core/registry/elecRegistry.java
@@ -0,0 +1,27 @@
+package elec332.core.registry;
+
+import elec332.core.handler.furnaceRecipehandler;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by Elec332 on 21-12-2014.
+ */
+public class elecRegistry {
+
+    public static void addSecondarySmelting(Block input, ItemStack output, float xp)
+    {
+        furnaceRecipehandler.smelting().func_151393_a(input, output, xp);
+    }
+
+    public static void addSecondarySmelting(Item input, ItemStack output, float xp)
+    {
+        furnaceRecipehandler.smelting().func_151396_a(input, output, xp);
+    }
+
+    public static void addSecondarySmelting(ItemStack input, ItemStack output, float xp)
+    {
+        furnaceRecipehandler.smelting().func_151394_a(input, output, xp);
+    }
+}
