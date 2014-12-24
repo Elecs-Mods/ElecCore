88c2dd701cfcfe49e62cd8be7e211d1987ce36eb
Elec332 <arnout1998@gmail.com>
2014-12-24 16:33:21 +0100

    Added list to StringHelper

--- src/main/java/elec332/core/helper/stringHelper.java
+++ src/main/java/elec332/core/helper/stringHelper.java
@@ -1,17 +1,24 @@
 package elec332.core.helper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Elec332 on 23-12-2014.
  */
 public class stringHelper {
 
-    public static ArrayList<String> convertStringArrayToArraylist(String[] Ores) {
+    public static ArrayList<String> convertStringArrayToArraylist(String[] string) {
         ArrayList<String> stringList = new ArrayList<String>();
-        for (String s : Ores) {
+        for (String s : string) {
             stringList.add(s);
         }
         return stringList;
+    }
+    public static String[] convertStringListToStringArray(List Arraylist){
+        String[] Def = new String[Arraylist.size()];
+        Arraylist.toArray(Def);
+
+        return Def;
     }
 }
