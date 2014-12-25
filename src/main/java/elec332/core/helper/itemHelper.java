2630288f67c440f85d493ebe21a6b5c58eb0bd89
Elec332 <arnout1998@gmail.com>
2014-12-25 13:11:24 +0100

    Add amount option for Items

--- src/main/java/elec332/core/helper/itemHelper.java
+++ src/main/java/elec332/core/helper/itemHelper.java
@@ -6,10 +6,15 @@ import net.minecraft.item.ItemStack;
  * Created by Elec332 on 23-12-2014.
  */
 public class itemHelper {
-    public static ItemStack getNuggetFormOre(String ore){
+
+    public static ItemStack getNuggetFromOre(String Ore){
+        return getNuggetFromOre(Ore, 1);
+    }
+
+    public static ItemStack getNuggetFromOre(String ore, int Amount){
         String nuggetName = ore.replace("ore", "nugget");
         try{
-            return new ItemStack(oredictHelper.getFirstOredictEntry(nuggetName),1 , oredictHelper.getFirstOredictItemDamage(nuggetName));
+            return new ItemStack(oredictHelper.getFirstOredictEntry(nuggetName),Amount , oredictHelper.getFirstOredictItemDamage(nuggetName));
         }catch (Exception e){
 
         }
--- src/main/java/elec332/core/helper/stringHelper.java
+++ src/main/java/elec332/core/helper/stringHelper.java
@@ -8,6 +8,7 @@ import java.util.List;
  */
 public class stringHelper {
 
+    @SuppressWarnings("unchecked")
     public static ArrayList<String> convertStringArrayToArraylist(String[] string) {
         ArrayList<String> stringList = new ArrayList<String>();
         for (String s : string) {
