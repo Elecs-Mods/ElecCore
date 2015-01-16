bb4f11ac4c0ad5bc5f155c25bcd91e70c78c69d5
Elec332 <arnout1998@gmail.com>
2015-01-16 18:19:49 +0100

    Split StringHelper and ArrayHelper

--- src/main/java/elec332/core/helper/stringHelper.java
+++ src/main/java/elec332/core/helper/stringHelper.java
@@ -16,24 +16,10 @@ public class stringHelper {
         }
         return stringList;
     }
-    public static String[] convertStringListToStringArray(List Arraylist){
+    public static String[] convertStringListToStringArray(ArrayList Arraylist){
         String[] Def = new String[Arraylist.size()];
         Arraylist.toArray(Def);
 
         return Def;
-    }
-
-    @SuppressWarnings("unchecked")
-    public static ArrayList mergeArrays(ArrayList arrayList1, ArrayList arrayList2){
-        ArrayList returnArraylist = new ArrayList();
-        for (int i = 0; i < arrayList1.size(); i++) {
-            returnArraylist.add(arrayList1.get(i));
-        }
-
-        for (int i = 0; i < arrayList2.size(); i++) {
-            returnArraylist.add(arrayList2.get(i));
-        }
-
-        return returnArraylist;
     }
 }
