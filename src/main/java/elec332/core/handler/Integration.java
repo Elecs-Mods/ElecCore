1fcd9aec3840f118712cef0912992e68b2083627
Elec332 <arnout1998@gmail.com>
2015-02-08 12:53:00 +0100

    Expand Integration

--- src/main/java/elec332/core/handler/Integration.java
+++ src/main/java/elec332/core/handler/Integration.java
@@ -8,14 +8,23 @@ import elec332.core.main.ElecCore;
  */
 public class Integration {
 
-    public static boolean NEIIntegration = (CFG("NotEnoughItems") && Loader.isModLoaded("NotEnoughItems"));
+    public static boolean NEIIntegration = IntegrationM("NotEnoughItems");
 
     public static void init(){
         if (!NEIIntegration)
-            ElecCore.instance.info("No NEI detected, skipping NEI integration...");
+            ElecCore.instance.info(SkipMessage("NEI"));
     }
 
+    static Boolean IntegrationM(String name){
+        return CFG(name) && Loader(name);
+    }
     static Boolean CFG(String name){
         return ElecCore.instance.config.isEnabled(name, true, "Integration");
+    }
+    static Boolean Loader(String name){
+        return Loader.isModLoaded(name);
+    }
+    static String SkipMessage(String string){
+        return string + " wasn't detected, skipping " + string + " integration...";
     }
 }
