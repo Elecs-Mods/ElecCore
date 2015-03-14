01fb721453138e0b58f98be45b2c69d36784a070
Elec332 <arnout1998@gmail.com>
2015-03-14 13:24:21 +0100

    Make AbsractPacket sent NBTdata

--- src/main/java/elec332/core/network/AbstractPacket.java
+++ src/main/java/elec332/core/network/AbstractPacket.java
@@ -1,24 +1,35 @@
 package elec332.core.network;
 
+import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.nbt.NBTTagCompound;
 
 /**
  * Created by Elec332 on 23-2-2015.
  */
 public abstract class AbstractPacket implements IMessage, IMessageHandler<AbstractPacket, IMessage> {
 
+    public AbstractPacket(){
+    }
+
+    public AbstractPacket(NBTTagCompound b){
+        this.networkPackageObject = b;
+    }
+
     @Override
     public void fromBytes(ByteBuf buf) {
+        this.networkPackageObject = ByteBufUtils.readTag(buf);
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
+        ByteBufUtils.writeTag(buf, this.networkPackageObject);
     }
 
-    public Object networkPackageObject;
+    public NBTTagCompound networkPackageObject;
 
     @Override
     public abstract IMessage onMessage(AbstractPacket message, MessageContext ctx);
