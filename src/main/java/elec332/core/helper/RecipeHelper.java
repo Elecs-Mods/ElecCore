9673631a05a6df6cd0a283ca87271f4d2716f752
Elec332 <arnout1998@gmail.com>
2015-04-02 21:56:28 +0200

    Expand RecipeHelper

--- src/main/java/elec332/core/helper/RecipeHelper.java
+++ src/main/java/elec332/core/helper/RecipeHelper.java
@@ -3,6 +3,7 @@ package elec332.core.helper;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 
 /**
@@ -10,20 +11,21 @@ import net.minecraft.item.crafting.IRecipe;
  */
 public class RecipeHelper {
 
-    public static IRecipe addStorageRecipe(ItemStack Input, Item Output){
-        return addStorageRecipe(Input, new ItemStack(Output));
-    }
-
-    public static IRecipe addStorageRecipe(Item Input, ItemStack Output){
-        return addStorageRecipe(new ItemStack(Input), Output);
-    }
-
-    public static IRecipe addStorageRecipe(Item Input, Item Output){
-        return addStorageRecipe(new ItemStack(Input), new ItemStack(Output));
+    public static IRecipe getCraftingRecipe(ItemStack itemStack) {
+        for(int i = 0; i < CraftingManager.getInstance().getRecipeList().size(); i++) {
+            IRecipe recipe = (IRecipe) CraftingManager.getInstance().getRecipeList().get(i);
+            if (recipe != null) {
+                ItemStack output = recipe.getRecipeOutput();
+                if (ItemHelper.areItemsEqual(itemStack, output)) {
+                    return recipe;
+                }
+            }
+        }
+        return null;
     }
 
     public static IRecipe addStorageRecipe(ItemStack Input, ItemStack Output){
-        return GameRegistry.addShapedRecipe(Output, new Object[]{
-                "III", "III", "III",'I',Input});
+        return GameRegistry.addShapedRecipe(Output,
+                "III", "III", "III",'I',Input);
     }
 }
