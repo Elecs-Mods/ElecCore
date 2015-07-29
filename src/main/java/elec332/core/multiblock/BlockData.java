73ba0baa32f15d2177442ac58cd1bed7c4c357f0
Elec332 <arnout1998@gmail.com>
2015-07-29 21:53:11 +0200

    Added wildcard value

--- src/main/java/elec332/core/multiblock/BlockData.java
+++ src/main/java/elec332/core/multiblock/BlockData.java
@@ -3,6 +3,7 @@ package elec332.core.multiblock;
 import elec332.core.minetweaker.MineTweakerHelper;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 /**
  * Created by Elec332 on 26-7-2015.
@@ -31,8 +32,12 @@ public final class BlockData {
         return meta;
     }
 
+    private boolean checkMeta(Object obj){
+        return obj.hashCode() == hashCode() || hashCode() == OreDictionary.WILDCARD_VALUE || obj.hashCode() == OreDictionary.WILDCARD_VALUE;
+    }
+
     @Override
     public boolean equals(Object obj) {
-        return obj instanceof BlockData && ((BlockData) obj).block.equals(block) && obj.hashCode() == hashCode();
+        return obj instanceof BlockData && ((BlockData) obj).block.equals(block) && checkMeta(obj);
     }
 }
