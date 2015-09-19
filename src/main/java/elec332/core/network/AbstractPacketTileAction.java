7279a1983c6f4dc0efcf6bab58ce517a5c9cdfa6
Elec332 <arnout1998@gmail.com>
2015-09-19 15:20:08 +0200

    Fixed NoClassDefFoundException, closes #4

--- src/main/java/elec332/core/network/AbstractPacketTileAction.java
+++ src/main/java/elec332/core/network/AbstractPacketTileAction.java
@@ -3,10 +3,10 @@ package elec332.core.network;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import elec332.core.util.BlockLoc;
 import elec332.core.util.NBTHelper;
 import elec332.core.world.WorldHelper;
-import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
@@ -31,11 +31,15 @@ public abstract class AbstractPacketTileAction extends AbstractPacket {
         BlockLoc loc = new BlockLoc(message.networkPackageObject);
         int i = message.networkPackageObject.getInteger("id");
         NBTTagCompound data = message.networkPackageObject.getCompoundTag("data");
-        if (ctx.side == Side.CLIENT) {
-            processPacket(WorldHelper.getTileAt(Minecraft.getMinecraft().theWorld, loc), i, data, ctx);
-            return null;
-        }
-        processPacket(WorldHelper.getTileAt(ctx.getServerHandler().playerEntity.getEntityWorld(), loc), i, data, ctx);
+        if (ctx.side == Side.CLIENT)
+            return processClient(loc, i, data, ctx);
+        processPacket(WorldHelper.getTileAt(ctx.getServerHandler().playerEntity.getServerForPlayer(), loc), i, data, ctx);
+        return null;
+    }
+
+    @SideOnly(Side.CLIENT)
+    private IMessage processClient(BlockLoc loc, int id, NBTTagCompound data, MessageContext ctx){
+        processPacket(WorldHelper.getTileAt(net.minecraft.client.Minecraft.getMinecraft().theWorld, loc), id, data, ctx);
         return null;
     }
 
