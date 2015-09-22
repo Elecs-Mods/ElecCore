34de1ff809d7501e7aeb881c39fb7ceb5eb20825
Elec332 <arnout1998@gmail.com>
2015-09-22 16:32:45 +0200

    Added AbstractBlockRenderer

---
+++ src/main/java/elec332/core/client/render/AbstractBlockRenderer.java
@@ -0,0 +1,101 @@
+package elec332.core.client.render;
+
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.IItemRenderer;
+
+/**
+ * Created by Elec332 on 22-9-2015.
+ */
+public abstract class AbstractBlockRenderer extends TileEntitySpecialRenderer implements ISimpleBlockRenderingHandler, IItemRenderer {
+
+    public AbstractBlockRenderer(Block block){
+        this(block, null);
+    }
+
+    public AbstractBlockRenderer(Class<? extends TileEntity> tileClass){
+        this(null, tileClass);
+    }
+
+    public AbstractBlockRenderer(Block block, Class<? extends TileEntity> tileClass){
+        this.block = block;
+        this.tileClass = tileClass;
+    }
+
+    protected final Block block;
+    final Class<? extends TileEntity> tileClass;
+
+    private int renderID = -1;
+    private final Tessellator tessellator = Tessellator.instance;
+
+    final void setRenderID(int renderID) {
+        if (this.renderID != -1)
+            throw new IllegalAccessError();
+        this.renderID = renderID;
+    }
+
+    @Override
+    public final boolean handleRenderType(ItemStack item, ItemRenderType type) {
+        return type != ItemRenderType.FIRST_PERSON_MAP;
+    }
+
+    @Override
+    public final boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
+        return true;
+    }
+
+    @Override
+    public final void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+        renderItem(item, type, (RenderBlocks) data[0]);
+    }
+
+    @Override
+    public final void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
+    }
+
+    @Override
+    public final boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+        return validForRendering(modelId) && doRenderInWorld(world, x, y, z, renderer, 0, false);
+    }
+
+    @Override
+    public final boolean shouldRender3DInInventory(int modelId) {
+        return false;
+    }
+
+    protected boolean validForRendering(int i){
+        return i == getRenderId();
+    }
+
+    @Override
+    public final int getRenderId() {
+        return renderID;
+    }
+
+    @Override
+    public final void renderTileEntityAt(TileEntity tile, double x, double y, double z, float partialTicks) {
+        doRenderInWorld(tile.getWorldObj(), x, y, z, RenderBlocks.getInstance(), partialTicks, true);
+    }
+
+    private boolean doRenderInWorld(IBlockAccess world, double x, double y, double z, RenderBlocks renderer, float partialTicks, boolean tesr){
+        tessellator.setColorRGBA_F(1, 1, 1, 1);
+        return renderBlockAt(world, x, y, z, renderer, partialTicks, tesr);
+    }
+
+    protected abstract boolean renderBlockAt(IBlockAccess world, double x, double y, double z, RenderBlocks renderer, float partialTicks, boolean tesr);
+
+    protected abstract void renderItem(ItemStack stack, ItemRenderType renderType, RenderBlocks renderer);
+
+    protected abstract boolean isISBRH();
+
+    protected abstract boolean isItemRenderer();
+
+    protected abstract boolean isTESR();
+
+}
--- src/main/java/elec332/core/client/render/RenderHelper.java
+++ src/main/java/elec332/core/client/render/RenderHelper.java
@@ -1,11 +1,20 @@
 package elec332.core.client.render;
 
+import com.google.common.collect.Lists;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.fluids.Fluid;
+
+import java.util.List;
 
 /**
  * Created by Elec332 on 31-7-2015.
@@ -13,6 +22,42 @@ import net.minecraftforge.fluids.Fluid;
 public class RenderHelper {
 
     public static final float renderUnit = 1/16f;
+    private static List<Block> itemRenders = Lists.newArrayList();
+    private static List<Block> blockRenders = Lists.newArrayList();
+    private static List<Class<? extends TileEntity>> tileRenders = Lists.newArrayList();
+
+    public static <A extends AbstractBlockRenderer> A registerBlockRenderer(A renderer){
+        if (renderer == null)
+            throw new IllegalArgumentException("Cannot register a null renderer!");
+        if (!renderer.isISBRH() && !renderer.isItemRenderer() && !renderer.isTESR())
+            throw new IllegalArgumentException("Useless render handler detected: "+renderer.getClass());
+        if ((renderer.isItemRenderer() || renderer.isISBRH())){
+            if (renderer.block == null)
+                throw new IllegalArgumentException("Cannot register IItemRenderer or ISBRH for a null Block!");
+            if (renderer.isItemRenderer() && itemRenders.contains(renderer.block) || renderer.isISBRH() && blockRenders.contains(renderer.block))
+                throw new IllegalArgumentException("Renderer already registered for "+renderer.block.getClass());
+        }
+        if (renderer.isTESR()) {
+            if (renderer.tileClass == null)
+                throw new IllegalArgumentException("Cannot register TESR for a null TileEntity class!");
+            if (tileRenders.contains(renderer.tileClass))
+                throw new IllegalArgumentException("Renderer already registered for "+renderer.tileClass);
+        }
+        if (renderer.isISBRH()){
+            renderer.setRenderID(RenderingRegistry.getNextAvailableRenderId());
+            RenderingRegistry.registerBlockHandler(renderer);
+            blockRenders.add(renderer.block);
+        }
+        if (renderer.isItemRenderer()){
+            MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(renderer.block), renderer);
+            itemRenders.add(renderer.block);
+        }
+        if (renderer.isTESR()){
+            ClientRegistry.bindTileEntitySpecialRenderer(renderer.tileClass, renderer);
+            tileRenders.add(renderer.tileClass);
+        }
+        return renderer;
+    }
 
     public static void bindBlockTextures(){
         bindTexture(getBlocksResourceLocation());
