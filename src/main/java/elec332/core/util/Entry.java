784f674e96ac8367fec25329834792466b7979d6
Elec332 <arnout1998@gmail.com>
2015-10-23 16:25:39 +0200

    Add Entry

---
+++ src/main/java/elec332/core/util/Entry.java
@@ -0,0 +1,38 @@
+package elec332.core.util;
+
+import java.util.Map;
+
+/**
+ * Created by Elec332 on 23-10-2015.
+ */
+public class Entry<K, V> implements Map.Entry<K, V> {
+
+    public static <K, V> Map.Entry<K, V> newEntry(K k, V v){
+        return new Entry<K, V>(k, v);
+    }
+
+    private Entry(K k, V v){
+        this.k = k;
+        this.v = v;
+    }
+
+    private final K k;
+    private V v;
+
+    @Override
+    public K getKey() {
+        return k;
+    }
+
+    @Override
+    public V getValue() {
+        return v;
+    }
+
+    @Override
+    public V setValue(V value) {
+        V old = this.v;
+        this.v = value;
+        return old;
+    }
+}
