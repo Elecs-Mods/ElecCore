497973efa68fdd5fe6e4f3906d81e04d4081f455
Elec332 <arnout1998@gmail.com>
2016-05-17 21:36:32 +0200

    Fixing...

---
+++ src/main/java/elec332/core/module/ISpecialMultiBlockCapabilityProvider.java
@@ -0,0 +1,16 @@
+package elec332.core.module;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.capabilities.Capability;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by Elec332 on 16-5-2016.
+ */
+public interface ISpecialMultiBlockCapabilityProvider {
+
+    public <T> T getSpecialCapability(Capability<T> capability, EnumFacing facing, @Nonnull BlockPos pos);
+
+}
