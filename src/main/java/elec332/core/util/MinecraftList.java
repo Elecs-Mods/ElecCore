74d0714247f2d7e3efc3cac7b956e4306a58cb11
Elec332 <arnout1998@gmail.com>
2017-04-16 22:23:40 +0200

    Fixed possible issue when attempting to clear an Array-List

--- src/main/java/elec332/core/util/MinecraftList.java
+++ src/main/java/elec332/core/util/MinecraftList.java
@@ -75,7 +75,7 @@ public final class MinecraftList<E> extends AbstractList<E> {
                 set(i, defaultObj);
             }
         //} else {
-            underlyingList.clear();
+            //underlyingList.clear();
         //}
     }
 
