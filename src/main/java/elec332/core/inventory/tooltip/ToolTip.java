198dbde707d566d25d8a0f5c3fddfbf275a0d7b6
Elec332 <arnout1998@gmail.com>
2019-03-01 15:08:23 +0100

    Fixed null stack being used, closes #144

--- src/main/java/elec332/core/inventory/tooltip/ToolTip.java
+++ src/main/java/elec332/core/inventory/tooltip/ToolTip.java
@@ -1,13 +1,17 @@
 package elec332.core.inventory.tooltip;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import elec332.core.client.RenderHelper;
+import elec332.core.util.ItemStackHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.client.config.GuiUtils;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,8 +55,13 @@ public class ToolTip {
 
     @SideOnly(Side.CLIENT)
     public void renderTooltip(int mouseX, int mouseY, int guiLeft, int guiTop) {
+        renderTooltip(mouseX, mouseY, guiLeft, guiTop, ItemStackHelper.NULL_STACK);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void renderTooltip(int mouseX, int mouseY, int guiLeft, int guiTop, @Nonnull ItemStack stack) {
         Minecraft mc = Minecraft.getMinecraft();
-        GuiUtils.drawHoveringText(null, tooltip, mouseX, mouseY, mc.displayWidth, mc.displayHeight, width, RenderHelper.getMCFontrenderer());
+        GuiUtils.drawHoveringText(Preconditions.checkNotNull(stack), tooltip, mouseX, mouseY, mc.displayWidth, mc.displayHeight, width, RenderHelper.getMCFontrenderer());
     }
 
     public static class ColouredString {
