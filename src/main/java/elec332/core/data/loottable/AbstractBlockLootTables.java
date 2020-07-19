f628654ed4c5d4cd369b09b249f38ee0eb658fa0
Elec332 <arnout1998@gmail.com>
2020-07-19 18:47:20 +0200

    Fixed object presence detection

--- src/main/java/elec332/core/data/loottable/AbstractBlockLootTables.java
+++ src/main/java/elec332/core/data/loottable/AbstractBlockLootTables.java
@@ -1,15 +1,36 @@
 package elec332.core.data.loottable;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.data.loot.BlockLootTables;
 import net.minecraft.util.IItemProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTable;
 
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Created by Elec332 on 16-7-2020
  */
 public abstract class AbstractBlockLootTables extends BlockLootTables {
+
+    public AbstractBlockLootTables() {
+        this(null);
+    }
+
+    public AbstractBlockLootTables(String modId) {
+        this.modId = modId;
+        lootTables = Maps.newHashMap();
+    }
+
+    private final String modId;
+    private final Map<ResourceLocation, LootTable.Builder> lootTables;
 
     @Override
     protected final void addTables() {
@@ -30,8 +51,29 @@ public abstract class AbstractBlockLootTables extends BlockLootTables {
         registerLootTable(block, func_218482_a());
     }
 
-    static {
+    @Override
+    public void accept(@Nonnull BiConsumer<ResourceLocation, LootTable.Builder> registry) {
+        if (modId == null) {
+            addTables();
+            lootTables.forEach(registry);
+        } else {
+            super.accept(registry);
+        }
+    }
 
+    @Nonnull
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return Lists.newArrayList(super.getKnownBlocks())
+                .stream()
+                .filter(b -> Preconditions.checkNotNull(b.getRegistryName()).getNamespace().equals(modId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void registerLootTable(@Nonnull Block blockIn, @Nonnull LootTable.Builder table) {
+        this.lootTables.put(blockIn.getLootTable(), table);
+        super.registerLootTable(blockIn, table);
     }
 
 }
--- src/main/java/elec332/core/data/loottable/AbstractEntityLootTables.java
+++ src/main/java/elec332/core/data/loottable/AbstractEntityLootTables.java
@@ -1,11 +1,34 @@
 package elec332.core.data.loottable;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.data.loot.EntityLootTables;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTable;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 /**
  * Created by Elec332 on 16-7-2020
  */
 public abstract class AbstractEntityLootTables extends EntityLootTables {
+
+    public AbstractEntityLootTables() {
+        this(null);
+    }
+
+    public AbstractEntityLootTables(String modId) {
+        this.modId = modId;
+        lootTables = Maps.newHashMap();
+    }
+
+    private final String modId;
+    private final Map<ResourceLocation, LootTable.Builder> lootTables;
 
     @Override
     protected final void addTables() {
@@ -13,5 +36,30 @@ public abstract class AbstractEntityLootTables extends EntityLootTables {
     }
 
     protected abstract void registerEntityTables();
+
+    @Override
+    public void accept(@Nonnull BiConsumer<ResourceLocation, LootTable.Builder> registry) {
+        if (modId == null) {
+            addTables();
+            lootTables.forEach(registry);
+        } else {
+            super.accept(registry);
+        }
+    }
+
+    @Nonnull
+    @Override
+    protected Iterable<EntityType<?>> getKnownEntities() {
+        return Lists.newArrayList(super.getKnownEntities())
+                .stream()
+                .filter(b -> Preconditions.checkNotNull(b.getRegistryName()).getNamespace().equals(modId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void registerLootTable(@Nonnull EntityType<?> type, @Nonnull LootTable.Builder table) {
+        this.lootTables.put(type.getLootTable(), table);
+        super.registerLootTable(type, table);
+    }
 
 }
